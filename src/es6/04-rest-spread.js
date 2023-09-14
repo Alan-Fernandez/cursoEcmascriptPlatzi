1661cb883fd4e1ff33914721667ec336de500d1b
AlphaEduc <92124271+Alan-Fernandez@users.noreply.github.com>
2023-09-14 18:48:28 -0300

    ES6: spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -21,3 +21,21 @@ console.log(username, age);
 
 // Opcion 2
 console.log(username, user.age);
+
+// sprad operator
+
+let person = { name: "Alexa", age: 24 };
+let country = "COL"
+
+let data = { ...person, country };
+console.log(data);
+
+//Rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3)
